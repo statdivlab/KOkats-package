b16ba70a9e139141ee2c0f67b3bd27b00600b021
Sarah Teichman <teichs@uw.edu>
2023-01-18 12:45:37 -0800

    one more helper fn

--- R/generate_Y_tilde_plus.R
+++ R/generate_Y_tilde_plus.R
@@ -1,14 +1,21 @@
-#' Generate Y plus 
-#' Generate an augmented outcome matrix to use with Firth penalized likelihood.
+#' Generate Y tilde plus 
+#' Generate an augmented outcome vector to use with Firth penalized likelihood.
 #' 
-#' @param Y_tilde_plus An augmented outcome vector of length \code{nJ}.
-#' @param J The number of KOs. 
+#' @param Y_tilde An outcome vector of length \code{nJ}.
+#' @param X_tilde An expanded version of the design matrix.
+#' @param W A diagonal matrix with expected values of Y tilde on the diagonal.
 #'  
-#' @return Y_plus, the augmented outcome matrix.
+#' @return Y_tilde_plus, the augmented outcome vector.
 #' 
 #' @export
-generate_Y_plus <- function(Y_tilde_plus, J) {
-  n <- length(Y_tilde_plus)/J
-  Y_plus <- matrix(Y_tilde_plus, nrow = n, ncol = J, byrow = TRUE) 
-  return(Y_plus)
+generate_Y_tilde_plus <- function(Y_tilde, X_tilde, W) {
+  info <- t(X_tilde) %*% W %*% X_tilde
+  info_chol <- Matrix::chol(info) 
+  chol_inv <- Matrix::solve(info_chol)
+  W_half <- sqrt(W)
+  # augmented portion 
+  aug <- diag(W_half %*% X_tilde %*% 
+                chol_inv %*% chol_inv %*% 
+                t(X_tilde) %*% W_half)
+  return(Y_tilde + aug/2)
 }
